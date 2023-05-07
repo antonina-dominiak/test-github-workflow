707dace9a4f550570d5bf9e2a462c2780ca02186
antonina-dominiak <antonina.dominiak01@gmail.com>
2023-05-07 18:56:53 +0200

    fixed the pylint errors (missing-final-newline, missing-module-docstring, import-error, missing-function-docstring)

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
+""" Aplikacja witajaca z Flask """
+
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """ Zwraca napis """
     return '<h1>Hello WSB! Greetings from Flask!</h1>'
+
+
 if __name__ == "__main__":
     app.run(debug=True)
